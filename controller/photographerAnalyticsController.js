1871a2d38e4508f74bf5503d0e6abf5009674f82
Harshit Srivastava <harshitlake161@gmail.com>
2025-01-02 18:59:30 +0530

    Added Total Sales in Analytics

--- controller/photographerAnalyticsController.js
+++ controller/photographerAnalyticsController.js
@@ -1,6 +1,8 @@
 const Photographer = require('../models/photographerModel.js')
 const ImageVault = require('../models/imagebase/imageVaultModel.js')
+const Order = require('../models/orderModel.js')
 const asyncHandler = require('express-async-handler')
+const mongoose = require('mongoose')
 
 const photographerDashboardData = asyncHandler(async (req, res) => {
     const { photographer } = req.query
@@ -8,7 +10,26 @@ const photographerDashboardData = asyncHandler(async (req, res) => {
     const totalUploadingImgCount = await ImageVault.countDocuments({ photographer })
     const pendingImagesCount = await ImageVault.countDocuments({ photographer, isActive: false })
 
-    res.status(200).send({ totalUploadingImgCount, pendingImagesCount })
+    let totalSales = await Order.aggregate([
+        {
+            $match: {
+                isPaid: true,
+                orderStatus: 'completed',
+                'imageInfo.photographer': new mongoose.Types.ObjectId(photographer)
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                sales: { $sum: '$imageInfo.price' }
+            }
+        }
+    ])
+    totalSales = totalSales.length > 0 ? totalSales[0]?.sales?.toFixed(2) : 0 
+
+   
+
+    res.status(200).send({ totalUploadingImgCount, pendingImagesCount, totalSales })
 })
 
 module.exports = {
