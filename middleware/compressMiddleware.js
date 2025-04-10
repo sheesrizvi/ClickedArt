5d19a83ed65a1bc543065e6c43e5b9c0b424955f
Harshit Srivastava <harshitlake161@gmail.com>
2025-04-10 12:39:16 +0530

    Story Compress Changes

--- middleware/compressMiddleware.js
+++ middleware/compressMiddleware.js
@@ -13,17 +13,16 @@ const s3 = new S3Client({
 });
 
 async function compressToExactSizeAndUpload(url, targetKB = 500) {
-
   const response = await axios.get(url, { responseType: 'arraybuffer' });
   const buffer = Buffer.from(response.data);
 
   const targetBytes = targetKB * 1024;
 
-
   const megapixels = targetBytes / (100 * 1024); 
   const side = Math.sqrt(megapixels) * 1000;
 
   const resizedBuffer = await sharp(buffer)
+    .rotate()
     .resize({
       width: Math.round(side),
       height: Math.round(side),
@@ -32,20 +31,19 @@ async function compressToExactSizeAndUpload(url, targetKB = 500) {
     })
     .toBuffer();
 
-
   const outputBuffer = await sharp(resizedBuffer)
-    .webp({
-      quality: 80,
-      effort: 6,
-      nearLossless: false,
-      smartSubsample: true,
-      lossless: false,
-      alphaQuality: 80,
-    })
-    .toBuffer();
+  .rotate() 
+  .webp({
+    quality: 80,
+    effort: 6,
+    nearLossless: false,
+    smartSubsample: true,
+    lossless: false,
+    alphaQuality: 80,
+  })
+  .toBuffer();
 
   console.log(`Final size: ${(outputBuffer.byteLength / 1024).toFixed(2)} KB`);
-
 
   const fileName = `${Date.now()}_${Math.round(Math.random() * 1e9)}.webp`;
 
