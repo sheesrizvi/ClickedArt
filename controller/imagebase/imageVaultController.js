3845e7d57acdcbebbbb076dd2b74777ea617c3cf
bhanu-sh <bhanu1234sharma@gmail.com>
2025-07-29 16:48:39 +0530

    feat: Enhance getImagesByEvents function to populate category and photographer details

--- controller/imagebase/imageVaultController.js
+++ controller/imagebase/imageVaultController.js
@@ -1216,7 +1216,7 @@ const getImagesOfEventsByPhotographer = asyncHandler(async (req, res) => {
 const getImagesByEvents= asyncHandler(async (req, res) => {
  const { eventName } = req.query
     
-    const images = await ImageVault.find({ eventName: eventName.toLowerCase() })
+    const images = await ImageVault.find({ eventName: eventName.toLowerCase() }).populate('category photographer')
 
     if(!images || images.length === 0) {
       throw new Error('Images not found')
