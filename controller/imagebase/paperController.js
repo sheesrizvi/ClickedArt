e5277903616e3f90068937e8928551f04b80fa38
Bhanu Sharma <bhanu1234sharma@gmail.com>
2025-10-03 11:38:02 +0530

    Refactor code structure for improved readability and maintainability

--- controller/imagebase/paperController.js
+++ controller/imagebase/paperController.js
@@ -1,9 +1,9 @@
 const Paper = require("../../models/imagebase/paperModel");
 const asyncHandler = require("express-async-handler");
-const ImageVault = require('../../models/imagebase/imageVaultModel');
+const ImageVault = require("../../models/imagebase/imageVaultModel");
 const { DeleteObjectCommand } = require("@aws-sdk/client-s3");
 const { S3Client } = require("@aws-sdk/client-s3");
-const Frame = require('../../models/imagebase/frameModel.js')
+const Frame = require("../../models/imagebase/frameModel.js");
 
 const config = {
   region: process.env.AWS_BUCKET_REGION,
@@ -15,7 +15,15 @@ const config = {
 const s3 = new S3Client(config);
 
 const createPaper = asyncHandler(async (req, res) => {
-  const { name, image, material, thickness, basePricePerSquareInch, customDimensions, photographerDiscount } = req.body;
+  const {
+    name,
+    image,
+    material,
+    thickness,
+    basePricePerSquareInch,
+    customDimensions,
+    photographerDiscount,
+  } = req.body;
 
   const existingPaper = await Paper.findOne({ name });
   if (existingPaper) {
@@ -30,7 +38,7 @@ const createPaper = asyncHandler(async (req, res) => {
     thickness,
     basePricePerSquareInch,
     customDimensions,
-    photographerDiscount
+    photographerDiscount,
   });
 
   res.status(201).send({ paper });
@@ -38,8 +46,12 @@ const createPaper = asyncHandler(async (req, res) => {
 
 const getPapers = asyncHandler(async (req, res) => {
   const { pageNumber = 1, pageSize = 20 } = req.query;
-  const papers = await Paper.find({}).sort({ createdAt: -1 }).skip((pageNumber - 1) * pageSize).limit(pageSize);
-  if (!papers || papers.length === 0) return res.status(400).send({ message: 'No Paper found' });
+  const papers = await Paper.find({})
+    .sort({ createdAt: -1 })
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize);
+  if (!papers || papers.length === 0)
+    return res.status(400).send({ message: "No Paper found" });
 
   const totalDocuments = await Paper.countDocuments({});
   const pageCount = Math.ceil(totalDocuments / pageSize);
@@ -48,41 +60,56 @@ const getPapers = asyncHandler(async (req, res) => {
 });
 
 const getPaperById = asyncHandler(async (req, res) => {
-  const { paperId } = req.query
+  const { paperId } = req.query;
   const paper = await Paper.findById(paperId);
   if (!paper) {
-    return res.status(404).send({ message: 'Paper not found' });
+    return res.status(404).send({ message: "Paper not found" });
   }
   res.status(200).json({ paper });
 });
 
 const updatePaper = asyncHandler(async (req, res) => {
-  const { paperId, name, image, material, thickness, basePricePerSquareInch, customDimensions, photographerDiscount } = req.body;
+  const {
+    paperId,
+    name,
+    image,
+    material,
+    thickness,
+    initialBasePrice,
+    basePricePerSquareInch,
+    customDimensions,
+    photographerDiscount,
+    userDiscount,
+  } = req.body;
 
   const paper = await Paper.findById(paperId);
   if (!paper) {
-    return res.status(404).send({ message: 'Paper not found' });
+    return res.status(404).send({ message: "Paper not found" });
   }
 
   paper.name = name ?? paper.name;
   paper.image = image ?? paper.image;
   paper.material = material ?? paper.material;
   paper.thickness = thickness ?? paper.thickness;
-  paper.basePricePerSquareInch = basePricePerSquareInch ?? paper.basePricePerSquareInch;
-  paper.photographerDiscount = photographerDiscount ?? paper.photographerDiscount
+  paper.initialBasePrice = initialBasePrice ?? paper.initialBasePrice;
+  paper.userDiscount = userDiscount ?? paper.userDiscount;
+  paper.basePricePerSquareInch =
+    basePricePerSquareInch ?? paper.basePricePerSquareInch;
+  paper.photographerDiscount =
+    photographerDiscount ?? paper.photographerDiscount;
   if (customDimensions) {
     paper.customDimensions = customDimensions;
   }
   const updatedPaper = await paper.save();
-  res.status(200).json({paper: updatedPaper});
-})
+  res.status(200).json({ paper: updatedPaper });
+});
 
 const deletePaper = asyncHandler(async (req, res) => {
-  const { paperId } = req.query
-  
+  const { paperId } = req.query;
+
   const paper = await Paper.findById(paperId);
   if (!paper) {
-    return res.status(404).send({ message: 'Paper not found' });
+    return res.status(404).send({ message: "Paper not found" });
   }
 
   const imageUrl = paper?.image || undefined;
@@ -105,19 +132,19 @@ const calculatePaperPrices = asyncHandler(async (req, res) => {
 
   if (!paperId || !imageId) {
     res.status(400);
-    throw new Error('Paper ID and Image ID are required.');
+    throw new Error("Paper ID and Image ID are required.");
   }
 
   const paper = await Paper.findById(paperId);
   if (!paper) {
     res.status(404);
-    throw new Error('Paper not found or is inactive.');
+    throw new Error("Paper not found or is inactive.");
   }
 
   const image = await ImageVault.findById(imageId);
   if (!image || !image.resolutions) {
     res.status(404);
-    throw new Error('Image not found or resolutions missing.');
+    throw new Error("Image not found or resolutions missing.");
   }
 
   const dpi = 300;
@@ -133,7 +160,11 @@ const calculatePaperPrices = asyncHandler(async (req, res) => {
   const prices = {};
   for (const [key, resolution] of Object.entries(image.resolutions)) {
     if (resolution.width && resolution.height) {
-      const price = calculatePriceForResolution(resolution.width, resolution.height, dpi);
+      const price = calculatePriceForResolution(
+        resolution.width,
+        resolution.height,
+        dpi
+      );
       prices[key] = {
         size: {
           width: parseFloat((resolution.width / dpi).toFixed(2)),
@@ -153,7 +184,7 @@ const calculatePaperPrices = asyncHandler(async (req, res) => {
   }));
 
   res.status(200).json({
-    message: 'Paper prices calculated successfully for all resolutions.',
+    message: "Paper prices calculated successfully for all resolutions.",
     data: {
       paperId: paper._id,
       imageId: image._id,
@@ -163,27 +194,26 @@ const calculatePaperPrices = asyncHandler(async (req, res) => {
       },
     },
   });
-})
-
+});
 
 const calculatePaperAndFramePrices = asyncHandler(async (req, res) => {
   const { paperId, imageId, frameId } = req.query;
 
   if (!paperId || !imageId) {
     res.status(400);
-    throw new Error('Paper ID and Image ID are required.');
+    throw new Error("Paper ID and Image ID are required.");
   }
 
   const paper = await Paper.findById(paperId);
   if (!paper) {
     res.status(404);
-    throw new Error('Paper not found or is inactive.');
+    throw new Error("Paper not found or is inactive.");
   }
 
   const image = await ImageVault.findById(imageId);
   if (!image || !image.resolutions) {
     res.status(404);
-    throw new Error('Image not found or resolutions missing.');
+    throw new Error("Image not found or resolutions missing.");
   }
 
   const dpi = 300;
@@ -199,7 +229,11 @@ const calculatePaperAndFramePrices = asyncHandler(async (req, res) => {
   const prices = {};
   for (const [key, resolution] of Object.entries(image.resolutions)) {
     if (resolution.width && resolution.height) {
-      const price = calculatePriceForResolution(resolution.width, resolution.height, dpi);
+      const price = calculatePriceForResolution(
+        resolution.width,
+        resolution.height,
+        dpi
+      );
       prices[key] = {
         size: {
           width: parseFloat((resolution.width / dpi).toFixed(2)),
@@ -218,44 +252,46 @@ const calculatePaperAndFramePrices = asyncHandler(async (req, res) => {
     price: dimension.price,
   }));
 
-// for frames calculation
+  // for frames calculation
 
-const frame = await Frame.findById(frameId);
+  const frame = await Frame.findById(frameId);
 
   if (!frame || !frame.isActive) {
     return res.status(200).send({
-      message: 'Paper Prices successfully calculated. No Frames found',
+      message: "Paper Prices successfully calculated. No Frames found",
       paperpricing: {
-      paperId: paper._id,
-      imageId: image._id,
-      pricing: {
-        resolutions: prices,
-        customDimensions: customPrices,
+        paperId: paper._id,
+        imageId: image._id,
+        pricing: {
+          resolutions: prices,
+          customDimensions: customPrices,
+        },
       },
-    },
-    })
+    });
   }
-
 
   const calculateFramePriceForResolution = (widthPixels, heightPixels, dpi) => {
     const widthInches = widthPixels / dpi;
     const heightInches = heightPixels / dpi;
 
-   
     const perimeterInches = 2 * (widthInches + heightInches);
 
-   
-    return Math.round(frame.basePricePerLinearInch * perimeterInches * 100) / 100;
+    return (
+      Math.round(frame.basePricePerLinearInch * perimeterInches * 100) / 100
+    );
   };
 
- 
   const pricesForFrame = {};
   for (const [key, resolution] of Object.entries(image.resolutions)) {
     if (resolution.width && resolution.height) {
       const widthInches = parseFloat((resolution.width / dpi).toFixed(2));
       const heightInches = parseFloat((resolution.height / dpi).toFixed(2));
 
-      const price = calculateFramePriceForResolution(resolution.width, resolution.height, dpi);
+      const price = calculateFramePriceForResolution(
+        resolution.width,
+        resolution.height,
+        dpi
+      );
       pricesForFrame[key] = {
         size: {
           width: widthInches,
@@ -266,7 +302,6 @@ const frame = await Frame.findById(frameId);
     }
   }
 
-
   const customPricesForFrame = frame.customDimensions.map((dimension) => ({
     size: {
       width: dimension.width,
@@ -276,7 +311,8 @@ const frame = await Frame.findById(frameId);
   }));
 
   res.status(200).json({
-    message: 'Paper and frame prices calculated successfully for all resolutions.',
+    message:
+      "Paper and frame prices calculated successfully for all resolutions.",
     data: {
       paperId: paper._id,
       imageId: image._id,
@@ -286,16 +322,20 @@ const frame = await Frame.findById(frameId);
       },
       framepricing: {
         resolutions: pricesForFrame,
-        customDimensions: customPricesForFrame
-      }
+        customDimensions: customPricesForFrame,
+      },
     },
   });
-})
+});
 
 const getInActivePapers = asyncHandler(async (req, res) => {
   const { pageNumber = 1, pageSize = 20 } = req.query;
-  const papers = await Paper.find({ active: false }).sort({ createdAt: -1 }).skip((pageNumber - 1) * pageSize).limit(pageSize);
-  if (!papers || papers.length === 0) return res.status(400).send({ message: 'No Paper found' });
+  const papers = await Paper.find({ active: false })
+    .sort({ createdAt: -1 })
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize);
+  if (!papers || papers.length === 0)
+    return res.status(400).send({ message: "No Paper found" });
 
   const totalDocuments = await Paper.countDocuments({ active: false });
   const pageCount = Math.ceil(totalDocuments / pageSize);
@@ -305,8 +345,12 @@ const getInActivePapers = asyncHandler(async (req, res) => {
 
 const getActivePapers = asyncHandler(async (req, res) => {
   const { pageNumber = 1, pageSize = 20 } = req.query;
-  const papers = await Paper.find({ active: true }).sort({ createdAt: -1 }).skip((pageNumber - 1) * pageSize).limit(pageSize);
-  if (!papers || papers.length === 0) return res.status(400).send({ message: 'No Paper found' });
+  const papers = await Paper.find({ active: true })
+    .sort({ createdAt: -1 })
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize);
+  if (!papers || papers.length === 0)
+    return res.status(400).send({ message: "No Paper found" });
 
   const totalDocuments = await Paper.countDocuments({ active: true });
   const pageCount = Math.ceil(totalDocuments / pageSize);
@@ -315,16 +359,16 @@ const getActivePapers = asyncHandler(async (req, res) => {
 });
 
 const updatePaperStatus = asyncHandler(async (req, res) => {
-  const { paperId, status = true } = req.body
-  
-  if(!paperId) {
-    return res.status(400).send({ message: 'Paper Id is required' })
+  const { paperId, status = true } = req.body;
+
+  if (!paperId) {
+    return res.status(400).send({ message: "Paper Id is required" });
   }
 
-  await Paper.findOne({ _id: paperId }, { active: status })
+  await Paper.findOne({ _id: paperId }, { active: status });
 
-  res.status(200).send({ message: 'Paper Status updated successfully' })
- })
+  res.status(200).send({ message: "Paper Status updated successfully" });
+});
 
 module.exports = {
   createPaper,
@@ -336,5 +380,5 @@ module.exports = {
   calculatePaperAndFramePrices,
   getInActivePapers,
   getActivePapers,
-  updatePaperStatus
-}
+  updatePaperStatus,
+};
--- models/imagebase/paperModel.js
+++ models/imagebase/paperModel.js
@@ -1,47 +1,53 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const paperSchema = new mongoose.Schema(
-    {
-      name: {
-        type: String,
-        required: true,
-      },
-      image: {
-        type: String
-      },
-      material: {
-        type: String,
-        required: true,
-      },
-      thickness: {
-        type: Number, 
-        required: true,
-        default: 20,
-      },
-      basePricePerSquareInch: {
-        type: Number, 
-        required: true,
-      },
-      photographerDiscount:{
-          type: Number,
-          required: false
-      },
-      customDimensions: [
-        {
-          width: { type: Number, required: true }, 
-          height: { type: Number, required: true }, 
-          price: { type: Number, required: true }, 
-        },
-      ],
-      active: {
-        type: Boolean,
-        default: true
-      }
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
-  )
-  
-module.exports = mongoose.model("Paper", paperSchema)
-  
+    image: {
+      type: String,
+    },
+    material: {
+      type: String,
+      required: true,
+    },
+    thickness: {
+      type: Number,
+      required: true,
+      default: 20,
+    },
+    initialBasePrice: {
+      type: Number,
+      required: true,
+    },
+    basePricePerSquareInch: {
+      type: Number,
+      required: true,
+    },
+    photographerDiscount: {
+      type: Number,
+      required: false,
+    },
+    userDiscount: {
+      type: Number,
+      required: false,
+    },
+    customDimensions: [
+      {
+        width: { type: Number, required: true },
+        height: { type: Number, required: true },
+        initialPrice: { type: Number, required: true },
+        price: { type: Number, required: true },
+      },
+    ],
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
 
-
+module.exports = mongoose.model("Paper", paperSchema);
